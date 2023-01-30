d097e242f576db0f1d2a69168d09a2ff67cb2561
antalexklok <119968579+antalexklok@users.noreply.github.com>
2023-01-30 10:39:34 +0300

    Update slaider.js

--- slaider.js
+++ slaider.js
@@ -20,7 +20,7 @@ function showSlides(n) {
   if (n < 1) {
     slideIndex = slides.length
   }
-  for (i=0; i < slideIndex.length; i++){
+  for (i=0; i < slides.length; i++){
     slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
